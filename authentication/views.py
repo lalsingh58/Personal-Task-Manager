6de1e83eab26b54eecedbc7f97b7be1bce320a4d
Lal singh <lalsingh503511@gmail.com>
2025-06-06 21:17:29 +0530

    signpu intialize

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
 
 # # Create your views here.
-# def login(request):
-#     return render(request,"login.html")
-
-# def logout(request):
-#     return render(request,'loggedout.html')
